2b02c50d6a780dff8aea7f39dab8230bd27ad512
berk <96010671+berk2k@users.noreply.github.com>
2025-05-17 15:37:41 +0300

    Update and rename Server.txt to Server.py

--- Server.py
+++ Server.py
@@ -2,38 +2,39 @@ from m5stack import *
 from m5ui import *
 from uiflow import *
 
+# Initialize P2P communication
 remoteInit()
+
+# Set background color to dark gray
 setScreenColor(0x222222)
 
-label0 = M5TextBox(116, 69, "label0", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+# Create text labels to display received data
+label0 = M5TextBox(116, 69, "Waiting for data...", lcd.FONT_Default, 0xFFFFFF, rotate=0)
 label1 = M5TextBox(116, 90, " ", lcd.FONT_Default, 0xFFFFFF, rotate=0)
 label2 = M5TextBox(116, 120, " ", lcd.FONT_Default, 0xFFFFFF, rotate=0)
 
 while True:
+    # Receive data via P2P
     received_data = getP2PData()
+    
     if received_data is not None:
-        #rgb.setColorAll(0xff0000)
-        #label0.setText(str(received_data))
-        parsed_data = received_data.split('|')  # Split the data using the '|' character
+        # Split received string by '|' character into components
+        parsed_data = received_data.split('|')
+        
         if len(parsed_data) == 3:
             received_bed_Id = parsed_data[0]
             received_patient_name = parsed_data[1]
             received_position = parsed_data[2]
+            
+            # Update labels with the received data
             label0.setText(str(received_bed_Id))
             label1.setText(str(received_patient_name))
             label2.setText(str(received_position))
         else:
-            print("Invalid data format received")
+            # If the data format is incorrect
+            print("Invalid data format received:", received_data)
     else:
+        # No data received, show waiting message
         label0.setText("No data received")
 
-    wait_ms(500)  # Adjust this according to your requirements
-
-
-
-
-
-
-
-
-
+    wait_ms(500)  # Wait 500 milliseconds before checking again
